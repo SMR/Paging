b410b6c5688c22665566ef7d97254b1df0d395a9
Samarjeet dubey <sdubey@aperotechnologies.com>
2016-10-11 19:44:45 +0530

    First Commit

---
+++ Paging_Swift/Paging_Swift/PageItemController.swift
@@ -0,0 +1,33 @@
+//
+//  PageItemController.swift
+//  Paging_Swift
+//
+//  Created by Samarjeet dubey on 08/10/16.
+//  Copyright © 2016 iSparshIT. All rights reserved.
+//
+
+import UIKit
+
+class PageItemController: UIViewController {
+    
+    // MARK: - Variables
+    var itemIndex: Int = 0
+    var imageName: String = "" {
+        
+        didSet {
+            
+            if let imageView = contentImageView {
+                imageView.image = UIImage(named: imageName)
+            }
+            
+        }
+    }
+    
+    @IBOutlet var contentImageView: UIImageView?
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contentImageView!.image = UIImage(named: imageName)
+    }
+}
---
+++ Paging_Swift/Paging_Swift/ViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ViewController.swift
+//  Paging_Swift
+//
+//  Created by Samarjeet dubey on 08/10/16.
+//  Copyright © 2016 iSparshIT. All rights reserved.
+//
+
+import UIKit
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l < r
+  case (nil, _?):
+    return true
+  default:
+    return false
+  }
+}
+
+fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l > r
+  default:
+    return rhs < lhs
+  }
+}
+
+
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+    
+    // MARK: - Variables
+    fileprivate var pageViewController: UIPageViewController?
+    
+    // Initialize it right away here
+    fileprivate let contentImages = ["nature_pic_1",
+                                 "nature_pic_2",
+                                 "nature_pic_3",
+                                 "nature_pic_4"]
+
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createPageViewController()
+        setupPageControl()
+    }
+    
+    fileprivate func createPageViewController() {
+        
+        let pageController = self.storyboard!.instantiateViewController(withIdentifier: "PageController") as! UIPageViewController
+        pageController.dataSource = self
+        
+        if contentImages.count > 0 {
+            let firstController = getItemController(0)!
+            let startingViewControllers = [firstController]
+            pageController.setViewControllers(startingViewControllers, direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+        }
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMove(toParentViewController: self)
+    }
+
+    fileprivate func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.gray
+        appearance.currentPageIndicatorTintColor = UIColor.white
+        appearance.backgroundColor = UIColor.darkGray
+    }
+    
+    // MARK: - UIPageViewControllerDataSource
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex+1 < contentImages.count {
+            return getItemController(itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+    
+    fileprivate func getItemController(_ itemIndex: Int) -> PageItemController? {
+        
+        if itemIndex < contentImages.count {
+            let pageItemController = self.storyboard!.instantiateViewController(withIdentifier: "ItemController") as! PageItemController
+            pageItemController.itemIndex = itemIndex
+            pageItemController.imageName = contentImages[itemIndex]
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Page Indicator
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return contentImages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+    // MARK: - Additions
+    
+    func currentControllerIndex() -> Int {
+        
+        let pageItemController = self.currentController()
+        
+        if let controller = pageItemController as? PageItemController {
+            return controller.itemIndex
+        }
+        
+        return -1
+    }
+ 
+    func currentController() -> UIViewController? {
+        
+        if self.pageViewController?.viewControllers?.count > 0 {
+            return self.pageViewController?.viewControllers![0]
+        }
+        
+        return nil
+    }
+
+}
+
